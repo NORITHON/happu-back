2966b41773edce8def75a910353dac2931381b47
Lchaerin <kivod02@postech.ac.kr>
2023-01-28 04:56:00 +0900

    필터 선택 고침

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const loginRouter = require('./login.js');
 const reservationRouter = require('./reservation.js');
 const activityRouter = require('./activity.js');
+const alterRouter = require('./alter.js');
 
 const app = express();
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use("/login", loginRouter)
 app.use("/reservation", reservationRouter);
 app.use("/activity", activityRouter);
+app.use("/alter", alterRouter);
 
 app.get("", (req, res) => {
   res.send("Hello Express!");
