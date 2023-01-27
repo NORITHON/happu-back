6b4b643c9699f6c733cdf6e9c8bfce409bf20dbd
Minkyung Kwon <bigbangkmk0829@gmail.com>
2023-01-28 02:21:14 +0900

    filter 구현 완료

--- server/activity.js
+++ server/activity.js
@@ -22,7 +22,7 @@ router.use(express.json());
 //유저별 필터 저장 --> 개인화 부분
 router.post('/preference', (req, res) => {
     const user_id = req.body.user_id; //user이름
-    db.query('SELECT learnig, travel, art, sports, foods, walk, chronic, breathe, surprised, people, diet FROM user WHERE login_id = ?', [user_id], function(error, results, fields) {
+    db.query('SELECT learn, travel, art, sports, foods, walk, chronic, breathe, surprised, people, diet FROM user WHERE login_id = ?', [user_id], function(error, results, fields) {
         if (error) {
             console.log("message: cannot get info from SQL");
             throw error;
@@ -35,12 +35,27 @@ router.post('/preference', (req, res) => {
 
 //filter 동작 구현
 router.post('/filter', (req, res) => {
+    //json 파일 형식
+    // { 
+    //     "learn":0,
+    //     "travel":1,
+    //     "art": 0,
+    //     "sports":0,
+    //     "foods":0,
+    //     "walk":1,
+    //     "chronic":0,
+    //     "breathe":0,
+    //     "surprised":0,
+    //     "people":0,
+    //     "diet":0  
+    //   }
+
     //1/2 화면 필터링
-    const learnNew = req.body.learnNew;
-    const trip = req.body.trip;
+    const learn = req.body.learn;
+    const travel = req.body.travel;
     const art = req.body.art;
-    const exercise = req.body.exercise;
-    const food = req.body.food;
+    const sports = req.body.sports;
+    const foods = req.body.foods;
     
     //2/2 화면 필터링
     const walk = req.body.walk;
@@ -49,17 +64,16 @@ router.post('/filter', (req, res) => {
     const surprised = req.body.surprised;
     const people = req.body.people;
     const diet = req.body.diet;
-
     
-    // db.query('SELECT name, description, location, platform, price FROM activities WHERE name = ?', [name], function(error, results, fields) {
-    //     if (error) {
-    //         console.log("message: cannot get info from SQL");
-    //         throw error;
-    //     }
-    //     console.log(JSON.parse(JSON.stringify(results[0])));
-    //     console.log("success!!");
-    //     res.send(JSON.parse(JSON.stringify(results[0])));
-    // });
+    db.query('SELECT name, platform, price FROM activities WHERE activities.learn = ? AND activities.travel = ? AND activities.art = ? AND activities.sprots = ? AND activities.foods = ? AND activities.walk = ? AND activities.chronic = ? AND activities.breathe = ? AND activities.surprised = ? AND activities.people = ? AND activities.diet = ?', [learn, travel, art, sports, foods, walk, chronic, breathe, surprised, people, diet], function(error, results, fields) {
+        if (error) {
+            console.log("message: cannot get info from SQL");
+            throw error;
+        }
+        console.log(JSON.parse(JSON.stringify(results)));
+        console.log("success!!");
+        res.send(JSON.parse(JSON.stringify(results)));
+    });
 });
 
 //액티비티 상세 화면
